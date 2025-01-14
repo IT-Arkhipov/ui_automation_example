3ad7ee662c788e4d7d24fa3a65591ca900f3c993
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2025-01-14 09:40:18 +0300

    init: basic check for elements: presence, visibility

--- selenium_framework/automationexercise/common/predicates.py
+++ selenium_framework/automationexercise/common/predicates.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
-from selenium_framework.config import driver_singleton as driver
+from selenium_framework.automationexercise.common import shared as s
 
 
 def is_present(locator: str):
-    return len(driver.find_elements(By.XPATH, locator)) > 0
+    return len(s.driver.find_elements(By.XPATH, locator)) > 0
 
 
 def is_absent(locator: str):
