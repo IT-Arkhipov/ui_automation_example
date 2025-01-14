45ce92b1fff5b84941c0fa853f4b6e6b57be2794
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2025-01-14 09:41:35 +0300

    init: MainPage in PageObject model

--- selenium_framework/automationexercise/pages/main_page.py
+++ selenium_framework/automationexercise/pages/main_page.py
@@ -1,14 +1,13 @@
-from selenium_framework.automationexercise.resources.locators import base_url
-
-from selenium_framework.config import driver_singleton as driver
+from selenium_framework.automationexercise.common.locators import base_url
+from selenium_framework.automationexercise.common import shared as s
 
 
 class MainPage:
     url = base_url
 
     def open(self):
-        driver.get(self.url)
-        assert driver.current_url == self.url
+        s.driver.get(self.url)
+        assert s.driver.current_url == self.url
 
 
 main_page = MainPage()
