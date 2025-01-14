8eca9277242e5eed2fc9868014556293f3958a59
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2025-01-14 18:09:55 +0300

    add archiving log files

--- selenium_framework/automationexercise/common/logger.py
+++ selenium_framework/automationexercise/common/logger.py
@@ -1,6 +1,7 @@
 import logging
-
 import os
+import shutil
+
 from datetime import datetime
 
 from selenium_framework.automationexercise import project_folder
@@ -9,7 +10,15 @@ log_folder = os.path.join(project_folder, "logs")
 if not os.path.exists(log_folder):
     os.makedirs(log_folder)
 
-log_file_name = os.path.join(log_folder, f"run_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
+log_file_name = os.path.join(log_folder, "_current.log")
+
+if os.path.exists(log_file_name):
+    backup_name = f"archive_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+    backup_log_file = os.path.join(log_folder, backup_name)
+    try:
+        shutil.move(log_file_name, backup_log_file)
+    except FileNotFoundError:
+        pass
 
 logger = logging.getLogger()
 
