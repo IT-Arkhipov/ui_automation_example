7d08dc46c868f81ba70365cdfbedc0220fa94472
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2025-01-14 17:51:20 +0300

    add logging for commands

--- selenium_framework/automationexercise/common/commands.py
+++ selenium_framework/automationexercise/common/commands.py
@@ -1,20 +1,25 @@
 from selenium.webdriver.remote.webelement import WebElement
 
 from selenium_framework.automationexercise.common import shared as s
+from selenium_framework.automationexercise.common.logger import logger
 from selenium.webdriver.support import expected_conditions as EC
 
 
 def get(url: str):
+    logger.info(f"Открытие страницы: {url}")
     s.driver.get(url)
 
 
 def url_to_be(url: str):
+    logger.info(f"Проверка url: {url}")
     s.wait.until(EC.url_to_be(url))
 
 
 def element(locator: tuple) -> WebElement:
+    logger.info(f"Поиск элемента по селектору: {locator[1]}")
     return s.wait.until(EC.visibility_of(s.driver.find_element(*locator)))
 
 
 def elements(locator: tuple[str, str]) -> list[WebElement]:
+    logger.info(f"Поиск элементов по селектору: {locator[1]}")
     return s.wait.until(EC.presence_of_all_elements_located(locator))
