3195f8c3b5b175083b0ce75e36ab00bf19e2398f
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2025-01-14 18:40:43 +0300

    check for passed in .env logging level and set default value

--- selenium_framework/automationexercise/common/settings.py
+++ selenium_framework/automationexercise/common/settings.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from faker import Faker
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
@@ -7,11 +9,17 @@ fake = Faker()
 load_dotenv()
 
 
+logging_level = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+
+
 class Settings(BaseSettings):
     user_name: str = fake.first_name()
     user_email: str = fake.company_email()
     password: str = fake.password()
-    logging_level: str = 'INFO'
+    logging_level: str = 'DEBUG'
 
 
 settings = Settings()
+settings.logging_level = settings.logging_level.upper()
+if settings.logging_level not in logging_level:
+    settings.logging_level = 'DEBUG'
