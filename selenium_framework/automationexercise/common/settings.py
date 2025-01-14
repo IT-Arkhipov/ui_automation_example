3ed6ce5ce739e4e304f1b9c5413ea71afdcce350
Andrei Arkhipov <135824709+IT-Arkhipov@users.noreply.github.com>
2025-01-14 18:27:25 +0300

    change: settings from .env

--- selenium_framework/automationexercise/common/settings.py
+++ selenium_framework/automationexercise/common/settings.py
@@ -1,12 +1,17 @@
 from faker import Faker
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
 
 fake = Faker()
+load_dotenv()
 
 
-class Settings:
+class Settings(BaseSettings):
     user_name: str = fake.first_name()
     user_email: str = fake.company_email()
     password: str = fake.password()
+    logging_level: str = 'INFO'
 
 
 settings = Settings()
